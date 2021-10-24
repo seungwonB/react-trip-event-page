74a21385865b86cae38efe722b83bc48dc99936b
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-24 23:39:29 +0900

    fix: add Home.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import axios from "axios";
-axios({
-  method: "get",
-  url: "https://f177470e-beca-4f6a-9c56-1afd36fe9484.mock.pstmn.io/location",
 
-  responseType: "json",
-}).then(function (response) {
-  console.log(response.data);
-});
 class App extends React.Component {
   constructor(props) {
     super(props);
