6278c7f9a25bf98094577df7ede7be4d64086a40
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-29 21:21:25 +0900

    fix: 상품 정보 추가

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -8,49 +8,33 @@ function Card() {
   const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
-    // axios({
-    //   method: "GET",
-    //   url: process.env.REACT_APP_PRODUCT_URL,
-    //   data: {},
-    // })
-    //   .then((res) => {
-    //     setUsers(res.data);
-    //     console.log(res);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //     throw new Error(error);
-    //   });
-
     const response = await axios.get(process.env.REACT_APP_PRODUCT_URL);
     setUsers(response.data.themes);
     console.log(response.data);
-    // const response = await axios({
-    //   method: "GET",
-    //   url: process.env.REACT_APP_PRODUCT_URL,
-    //   data: {},
-    // }).then((res) => {
-    //   console.log(res.data.themes);
-    //   setUsers(res.data.themes);
-    // });
   };
-
   useEffect(() => {
     fetchUsers();
   }, []);
   return (
-    <div>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.trips[user.id].ticket_type},
-          {user.trips[user.id].discount_percent}, [
-          {user.trips[user.id].location}],
-          {user.trips[user.id].title},{user.trips[user.id].price_origin},
-          {user.trips[user.id].price_discounted},
-        </li>
-      ))}
-    </div>
+    <Container>
+      {users.map((user) => {
+        return (
+          <Product key={user.trips.id}>
+            {user.trips[user.id].ticket_type},
+            {user.trips[user.id].discount_percent}, [
+            {user.trips[user.id].location}],
+            {user.trips[user.id].title},{user.trips[user.id].price_origin},
+            {user.trips[user.id].price_discounted}
+          </Product>
+        );
+      })}
+    </Container>
   );
 }
 
+const Container = styled.div``;
+
+const Product = styled.div`
+  border: 1px solid black;
+`;
 export default Card;
