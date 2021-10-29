8cc04df53921d806bea8ce63f65dd954030b7c42
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-29 21:20:13 +0900

    fix: Route 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
+import Card from "./pages/Card";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   // eslint-disable-next-line react/require-render-return
   render() {
     return (
       <div>
         <Home />
+        <Card />
       </div>
     );
   }
