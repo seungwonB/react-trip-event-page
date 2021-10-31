6208a36a7d779bb5fb03ba52e9c6522b7670b475
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-31 23:42:25 +0900

    add: family, active 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import Home from "./pages/Home";
 import Card from "./pages/Card";
+import Family from "./pages/Family";
+import Active from "./pages/Active";
+import styled from "styled-components";
+import { withRouter } from "react-router";
+import { Route, Link } from "react-router-dom";
 
 class App extends React.Component {
-  // eslint-disable-next-line react/require-render-return
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div>
-        <Home />
-        <Card />
+        <Container>
+          <Route excat path="/" component={Home} />
+          <Route path="/card" component={Card} />
+          <Route path="/active" component={Active} />
+          <Route path="/family" component={Family} />
+        </Container>
       </div>
     );
   }
 }
 
-export default App;
+const Container = styled.div`
+  background-color: #b4e5ff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100vw;
+  // height: 100%;
+  margin: 0px auto;
+  max-width: 400px;
+  border: 1px solid b4e5ff;
+  padding: 0vh 16px;
+  padding-top: 10px;
+  font-family: "NanumRegular";
+`;
+
+export default withRouter(App);
